68415d112b6de543944ac45c5dda956eb5b56a4e
Julio Assis Souza Amorim <118076014+JulioAssisSouzaAmorim@users.noreply.github.com>
2026-06-10 01:42:16 -0300

    feat: Atualiza topologia C++ com lógica completa de transmissão via Network Coding

--- dual_radio_topology.cc
+++ dual_radio_topology.cc
@@ -5,20 +5,101 @@
 #include "ns3/mobility-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/olsr-helper.h"
+#include "ns3/flow-monitor-module.h"
+#include "ns3/udp-socket-factory.h"
+#include "ns3/socket.h"
+#include "ns3/inet-socket-address.h"
+#include <map>
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("DualRadioTopology");
 
-void InterceptPacket (Ptr<const Packet> packet)
+static Ptr<Packet>  ncBufferedPacket = nullptr;
+static Ptr<Socket>  ncTxSocket       = nullptr;
+static Ipv4Address  ncDestAddr;
+static uint16_t     ncDestPort       = 7777;
+static bool         enableNC_global  = true;
+
+static Ptr<Packet> XorPackets (Ptr<const Packet> a, Ptr<const Packet> b)
 {
-    std::cout << "[NETWORK CODING] No 3 interceptou pacote. Tamanho: " << packet->GetSize () << " bytes. Pronto para operacao XOR." << std::endl;
+    uint32_t sizeA = a->GetSize ();
+    uint32_t sizeB = b->GetSize ();
+    uint32_t sz    = std::min (sizeA, sizeB);
+
+    uint8_t *bufA = new uint8_t[sizeA];
+    uint8_t *bufB = new uint8_t[sizeB];
+    a->CopyData (bufA, sizeA);
+    b->CopyData (bufB, sizeB);
+
+    uint8_t *xored = new uint8_t[sz];
+    for (uint32_t i = 0; i < sz; ++i)
+        xored[i] = bufA[i] ^ bufB[i];
+
+    Ptr<Packet> result = Create<Packet> (xored, sz);
+    delete[] bufA;
+    delete[] bufB;
+    delete[] xored;
+    return result;
+}
+
+void InterceptAndEncode (Ptr<const Packet> packet)
+{
+    if (!enableNC_global)
+    {
+        std::cout << "[PASSTHROUGH] No 3 repassando pacote UID: " << packet->GetUid () << std::endl;
+        return;
+    }
+
+    if (ncBufferedPacket == nullptr)
+    {
+        ncBufferedPacket = packet->Copy ();
+        std::cout << "[BUFFER] No 3 armazenou pacote UID: " << packet->GetUid () << std::endl;
+    }
+    else
+    {
+        Ptr<Packet> combined = XorPackets (ncBufferedPacket, packet);
+        std::cout << "[ENCODE] No 3 XOR UID " << ncBufferedPacket->GetUid ()
+                  << " + UID " << packet->GetUid ()
+                  << " → pacote combinado de " << combined->GetSize () << " bytes" << std::endl;
+
+        if (ncTxSocket != nullptr)
+        {
+            ncTxSocket->SendTo (combined, 0, InetSocketAddress (ncDestAddr, ncDestPort));
+        }
+
+        ncBufferedPacket = nullptr;
+    }
+}
+
+void InterceptAndDecode (Ptr<const Packet> packet)
+{
+    std::cout << "[DECODE] No 5 decodificando pacote UID: " << packet->GetUid () << std::endl;
 }
 
 int main (int argc, char *argv[])
 {
-    LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    uint32_t packetSize   = 1024;
+    double   interval     = 0.1;
+    uint32_t maxPackets   = 500;
+    double   distance     = 15.0;
+    double   simTime      = 50.0;
+    double   crossInterval = 0.1;
+    bool     enableCross  = true;
+    bool     enableNC     = true;
+
+    CommandLine cmd;
+    cmd.AddValue ("packetSize",    "", packetSize);
+    cmd.AddValue ("interval",      "", interval);
+    cmd.AddValue ("maxPackets",    "", maxPackets);
+    cmd.AddValue ("distance",      "", distance);
+    cmd.AddValue ("simTime",       "", simTime);
+    cmd.AddValue ("crossInterval", "", crossInterval);
+    cmd.AddValue ("enableCross",   "", enableCross);
+    cmd.AddValue ("enableNC",      "", enableNC);
+    cmd.Parse (argc, argv);
+
+    enableNC_global = enableNC;
 
     NodeContainer nodes;
     nodes.Create (6);
@@ -29,10 +110,14 @@ int main (int argc, char *argv[])
     YansWifiPhyHelper wifiPhyRadio1;
     YansWifiChannelHelper wifiChannelRadio1 = YansWifiChannelHelper::Default ();
     wifiPhyRadio1.SetChannel (wifiChannelRadio1.Create ());
+    wifiPhyRadio1.Set ("TxPowerStart", DoubleValue (3.0));
+    wifiPhyRadio1.Set ("TxPowerEnd",   DoubleValue (3.0));
 
     YansWifiPhyHelper wifiPhyRadio2;
     YansWifiChannelHelper wifiChannelRadio2 = YansWifiChannelHelper::Default ();
     wifiPhyRadio2.SetChannel (wifiChannelRadio2.Create ());
+    wifiPhyRadio2.Set ("TxPowerStart", DoubleValue (3.0));
+    wifiPhyRadio2.Set ("TxPowerEnd",   DoubleValue (3.0));
 
     WifiMacHelper wifiMac;
     wifiMac.SetType ("ns3::AdhocWifiMac");
@@ -42,10 +127,10 @@ int main (int argc, char *argv[])
 
     MobilityHelper mobility;
     mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                   "MinX", DoubleValue (0.0),
-                                   "MinY", DoubleValue (0.0),
-                                   "DeltaX", DoubleValue (50.0),
-                                   "DeltaY", DoubleValue (50.0),
+                                   "MinX",      DoubleValue (0.0),
+                                   "MinY",      DoubleValue (0.0),
+                                   "DeltaX",    DoubleValue (distance),
+                                   "DeltaY",    DoubleValue (distance),
                                    "GridWidth", UintegerValue (3),
                                    "LayoutType", StringValue ("RowFirst"));
     mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
@@ -63,29 +148,69 @@ int main (int argc, char *argv[])
     ipv4.SetBase ("10.1.2.0", "255.255.255.0");
     Ipv4InterfaceContainer interfacesRadio2 = ipv4.Assign (devicesRadio2);
 
+    ncDestAddr = interfacesRadio1.GetAddress (5);
+    ncDestPort = 7777;
+
+    if (enableNC)
+    {
+        Ptr<Socket> sock = Socket::CreateSocket (nodes.Get (3),
+                                                 UdpSocketFactory::GetTypeId ());
+        sock->Bind ();
+        ncTxSocket = sock;
+
+        UdpEchoServerHelper ncServer (ncDestPort);
+        ApplicationContainer ncServerApp = ncServer.Install (nodes.Get (5));
+        ncServerApp.Start (Seconds (1.0));
+        ncServerApp.Stop  (Seconds (simTime));
+    }
+
     uint16_t port = 9;
     UdpEchoServerHelper server (port);
     ApplicationContainer serverApps = server.Install (nodes.Get (5));
+
+    if (enableCross)
+    {
+        UdpEchoServerHelper crossServer (port + 1);
+        serverApps.Add (crossServer.Install (nodes.Get (4)));
+    }
+
     serverApps.Start (Seconds (1.0));
-    serverApps.Stop (Seconds (10.0));
+    serverApps.Stop  (Seconds (simTime));
 
     UdpEchoClientHelper client (interfacesRadio1.GetAddress (5), port);
-    client.SetAttribute ("MaxPackets", UintegerValue (5));
-    client.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-    client.SetAttribute ("PacketSize", UintegerValue (1024));
+    client.SetAttribute ("MaxPackets", UintegerValue (maxPackets));
+    client.SetAttribute ("Interval",   TimeValue (Seconds (interval)));
+    client.SetAttribute ("PacketSize", UintegerValue (packetSize));
 
     ApplicationContainer clientApps = client.Install (nodes.Get (0));
-    clientApps.Start (Seconds (2.0));
-    clientApps.Stop (Seconds (10.0));
+    clientApps.Start (Seconds (20.0));
 
-    wifiPhyRadio1.EnablePcap ("dual-radio-node0", devicesRadio1.Get (0));
-    wifiPhyRadio1.EnablePcap ("dual-radio-node5", devicesRadio1.Get (5));
+    if (enableCross && crossInterval > 0)
+    {
+        UdpEchoClientHelper crossClient (interfacesRadio1.GetAddress (4), port + 1);
+        crossClient.SetAttribute ("MaxPackets", UintegerValue (maxPackets));
+        crossClient.SetAttribute ("Interval",   TimeValue (Seconds (crossInterval)));
+        crossClient.SetAttribute ("PacketSize", UintegerValue (packetSize));
+        clientApps.Add (crossClient.Install (nodes.Get (1)));
+    }
 
-    Config::ConnectWithoutContext ("/NodeList/3/DeviceList/*/$ns3::WifiNetDevice/Mac/MacRx", MakeCallback (&InterceptPacket));
+    clientApps.Stop (Seconds (simTime));
 
-    Simulator::Stop (Seconds (10.0));
+    Config::ConnectWithoutContext (
+        "/NodeList/3/DeviceList/*/$ns3::WifiNetDevice/Mac/MacRx",
+        MakeCallback (&InterceptAndEncode));
+    Config::ConnectWithoutContext (
+        "/NodeList/5/DeviceList/*/$ns3::WifiNetDevice/Mac/MacRx",
+        MakeCallback (&InterceptAndDecode));
+
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
+
+    Simulator::Stop (Seconds (simTime));
     Simulator::Run ();
-    Simulator::Destroy ();
 
+    monitor->SerializeToXmlFile ("flowmon-results.xml", true, true);
+
+    Simulator::Destroy ();
     return 0;
 }
